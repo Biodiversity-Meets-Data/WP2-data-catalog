fe6694c278d2f3b1303e2a986b9833f9ed3a759a
Olivier Martin <olivier.martin@sib.swiss>
2026-09-08 13:55:51 +0200

    geonetwork: modify query (switch from dev to prod, update url, no need for authentication)

--- openeo/src/test_geonetwork.py
+++ openeo/src/test_geonetwork.py
@@ -1,8 +1,9 @@
-# see https://docs.geonetwork-opensource.org/4.2/api/the-geonetwork-api/#connecting-to-the-api-with-python
+# soilgrids has an uuid of 8315df49-bde3-4138-8f71-9b722f3afd06
 import argparse
 import logging
 import requests
 
+# setup logging
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
@@ -11,46 +12,16 @@ parser = argparse.ArgumentParser(
     prog='geonetwork query',
     description='extract relevant data from a geonetwork catalogue')
 # setup arguments
-parser.add_argument('-d', '--dataset', required=True, help="uuid for dataset")
-parser.add_argument('-p', '--password', required=True, help="password for geonetwork catalogue")
-parser.add_argument('-u', '--username', required=True, help="username for geonetwork catalogue")
+parser.add_argument('-d', '--uuid', required=True, help="uuid for dataset")
 # parse
 args = parser.parse_args()
-username = args.username
-password = args.password
 
-# Set up your server and the authentication URL:
-server = "https://metadatacatalogue.lifewatch.dev"
-authenticate_url = server + "/srv/api/me"
-# authenticate_url = server + "/srv/eng/info?type=me"
-logger.info(f"query url: {authenticate_url}")
-
-# To generate the XRSF token, send a post request to the following URL: <server><authenticate_url>
-session = requests.Session()
-response = session.get(authenticate_url, auth=(username, password))
-# response = session.post(authenticate_url)
-
-response.raise_for_status()
-xsrf_token = session.cookies.get("XSRF-TOKEN")
-
-# Extract XRSF token
-# xsrf_token = response.cookies.get("XSRF-TOKEN")
-if xsrf_token:
-    logger.info(f"The XSRF Token is: {xsrf_token}")
-else:
-    logger.info("Unable to find the XSRF token")
-
-# Set header for connection
-headers = {
-    'Accept': 'application/json',
-    'X-XSRF-TOKEN': xsrf_token
-}
-
-query_url = server + f"/srv/api/records/{args.dataset}/formatters/json"
+# Set up your server and the query URL:
+server = "https://metadatacatalogue.lifewatch.eu"
+query_url = server + f"/srv/api/records?uuid={args.uuid}"
+logger.info(f"query url: {query_url}")
 
 # Send a put request to the endpoint
-response = session.get(query_url,
-                       auth=(username, password),
-                       headers=headers)
+response = requests.get(query_url)
 
-logger.info(response)
+logger.info(response.json())
